2448146e82c4b3b354909ec16fb46d4977f589f2
chibiegg <chibiegg@chibiegg.net>
2014-01-22 23:46:10 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Pytlite',
+      version='0.1.0',
+      description='Control Patlite Signal Tower from Python',
+      author='chibiegg',
+      author_email='git@chibiegg.net',
+      url='http://www.chibiegg.net/',
+      packages=["pytlite"],
+      )
